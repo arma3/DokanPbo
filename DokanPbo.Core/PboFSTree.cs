03492248762b7518a418a8c400c1240c69b7a9b4
Björn Dahlgren <bjorn@dahlgren.at>
2016-05-21 16:32:44 +0200

    Use a lookup table for the file tree

--- DokanPbo.Core/PboFSTree.cs
+++ DokanPbo.Core/PboFSTree.cs
@@ -10,6 +10,7 @@ namespace DokanPbo
     {
         private ArchiveManager archiveManager;
         private PboFSFolder root;
+        private Dictionary<string, PboFSNode> fileTreeLookup;
 
         public PboFSTree(ArchiveManager archiveManager)
         {
@@ -20,46 +21,31 @@ namespace DokanPbo
 
         public IList<FileInformation> FilesForPath(string path)
         {
-            if (path == "\\")
+            PboFSNode node = this.fileTreeLookup[path];
+            if (node.GetType() == typeof(PboFSFolder))
             {
-                return this.root.Children.Values.Select(f => f.FileInformation).ToList();
+                return ((PboFSFolder) node).Children.Values.Select(f => f.FileInformation).ToList();
             }
 
-            var currentFolder = this.root;
-            var paths = path.Split('\\');
-
-            for (int i = 1; i < paths.Length; i++)
-            {
-                var folderName = paths[i];
-                currentFolder = (PboFSFolder) currentFolder.Children[folderName];
-            }
-
-            return currentFolder.Children.Values.Select(f => f.FileInformation).ToList();
+            return new FileInformation[0];
         }
 
         public FileInformation FileInfoForPath(string path)
         {
-            if (path == "\\")
+            PboFSNode node = this.fileTreeLookup[path];
+            if (this.fileTreeLookup.TryGetValue(path, out node))
             {
-                return this.root.FileInformation;
+                return node.FileInformation;
             }
 
-            var currentFolder = this.root;
-            var paths = path.Split('\\');
-
-            for (int i = 1; i < paths.Length - 1; i++)
-            {
-                var folderName = paths[i];
-                currentFolder = (PboFSFolder)currentFolder.Children[folderName];
-            }
-
-            var fileName = paths[paths.Length - 1];
-            return currentFolder.Children[fileName].FileInformation;
+            return new FileInformation();
         }
 
         private void createFileTree()
         {
             this.root = new PboFSFolder(null);
+            this.fileTreeLookup = new Dictionary<string, PboFSNode>();
+            this.fileTreeLookup["\\"] = this.root;
 
             foreach (string filePath in this.archiveManager.FilePathToArchive.Keys)
             {
@@ -67,22 +53,35 @@ namespace DokanPbo
                 FileEntry file = this.archiveManager.FilePathToFileEntry[filePath];
 
                 PboFSFolder currentFolder = root;
+                var currentPath = "\\";
                 var paths = filePath.Split('\\');
 
                 // Create folder for all sub paths
                 for (int i = 1; i < paths.Length - 1; i++)
                 {
                     var folderName = paths[i];
+                    currentPath += folderName;
 
-                    if (!currentFolder.Children.ContainsKey(folderName)) {
-                        currentFolder.Children[folderName] = new PboFSFolder(folderName);
+                    PboFSFolder folder = null;
+                    if (!this.fileTreeLookup.ContainsKey(currentPath))
+                    {
+                        this.fileTreeLookup[currentPath] = new PboFSFolder(folderName);
+                    }
+                    folder = (PboFSFolder) this.fileTreeLookup[currentPath];
+
+                    if (!currentFolder.Children.ContainsKey(folderName))
+                    {
+                        currentFolder.Children[folderName] = folder;
                     }
 
                     currentFolder = (PboFSFolder) currentFolder.Children[folderName];
+                    currentPath += "\\";
                 }
 
                 var fileName = paths[paths.Length - 1];
-                currentFolder.Children[fileName] = new PboFSFile(fileName, archive, file);
+                var fileNode = new PboFSFile(fileName, archive, file);
+                currentFolder.Children[fileName] = fileNode;
+                this.fileTreeLookup[filePath] = fileNode;
             }
         }
     }
